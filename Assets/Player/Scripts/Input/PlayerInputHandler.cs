bd82add443aa2d4524789f8efc00c2c93be03b3f
NowakArtur97 <nowart97@gmail.com>
2021-02-16 17:02:17 +0100

    Setup controls for placing soul

--- Assets/Player/Scripts/Input/PlayerInputHandler.cs
+++ Assets/Player/Scripts/Input/PlayerInputHandler.cs
@@ -3,8 +3,17 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerInputHandler : MonoBehaviour
 {
+    [SerializeField]
+    private float _inputHoldTime = 0.2f;
+
     public Vector2 RawMovementInput { get; private set; }
     public bool BombPlacedInput { get; private set; }
+    private float _bombPlacedInputStartTime;
+
+    private void Update()
+    {
+        CheckBombPlacedInput();
+    }
 
     public void OnMoveInput(CallbackContext context)
     {
@@ -16,8 +25,13 @@ public class PlayerInputHandler : MonoBehaviour
         if (context.started)
         {
             BombPlacedInput = true;
+            _bombPlacedInputStartTime = Time.time;
         }
-        if (context.canceled)
+    }
+
+    private void CheckBombPlacedInput()
+    {
+        if (Time.time >= _bombPlacedInputStartTime + _inputHoldTime)
         {
             BombPlacedInput = false;
         }
