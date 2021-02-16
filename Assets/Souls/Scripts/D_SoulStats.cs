48d6e65ddf30cc84b0726abba0a0a89f66864278
NowakArtur97 <nowart97@gmail.com>
2021-02-16 16:12:15 +0100

    Create scriptable object for Soul Stats

---
+++ Assets/Souls/Scripts/D_SoulStats.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "_SoulStatsData", menuName = "Data/Soul/Stats")]
+public class D_SoulStats : ScriptableObject
+{
+    public int explosionRange;
+    public float timeToExplode;
+}
