cde68f6f73fc6fc75f4b2e835bf3a34d6454a3cf
NowakArtur97 <nowart97@gmail.com>
2021-03-18 09:36:16 +0100

    Lock Water Soul to Player

--- Assets/Souls/Scripts/SoulWithPlayerBuff.cs
+++ Assets/Souls/Scripts/SoulWithPlayerBuff.cs
@@ -2,16 +2,33 @@ using UnityEngine;
 
 public abstract class SoulWithPlayerBuff : Soul
 {
-    private Vector2 _playerPosition;
+    private GameObject _playerAliveGameObject;
+
+    private void Start()
+    {
+        _playerAliveGameObject = FindObjectOfType<Player>().transform.Find("Alive").gameObject;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        LockToPlayerPosition();
+    }
 
     protected override void UseAbility()
     {
         SoulAbility ability = Instantiate(SoulAbility, GetSoulPosition(), GetSoulRotation());
 
         ability.GetComponentInChildren<Animator>().SetBool(GetAnimationBoolName(), true);
+
+        ability.transform.parent = _playerAliveGameObject.transform;
     }
 
-    protected override Vector2 GetSoulPosition() => FindObjectOfType<Player>().transform.position;
+    protected override Vector2 GetSoulPosition() => _playerAliveGameObject.transform.position;
 
     protected override Quaternion GetSoulRotation() => Quaternion.Euler(0, 0, -90 * AbilityDirectionIndex);
+
+    private void LockToPlayerPosition() =>
+        transform.position = new Vector2(_playerAliveGameObject.transform.position.x, _playerAliveGameObject.transform.position.y) + SoulStats.startPositionOffset;
 }
