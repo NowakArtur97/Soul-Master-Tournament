c609005d17878d1c4fac3c76b5b9b95001308fdf
NowakArtur97 <nowart97@gmail.com>
2021-03-29 07:24:42 +0200

    Create Environment Hazard Stats scriptable object

---
+++ Assets/Environment Hazards/Scripts/D_EnvironmentHazardStats.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "_EnvironmentHazardData", menuName = "Data/EnvironmentHazard/Data")]
+public class D_EnvironmentHazardStats : ScriptableObject
+{
+    public float idleTime;
+    public float damage;
+}
