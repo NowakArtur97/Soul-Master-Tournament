84ae4a11c810962afe80d9db4555a14b8b047ced
NowakArtur97 <nowart97@gmail.com>
2021-04-02 09:42:04 +0200

    Create Ballista script

---
+++ Assets/Environment Hazards/Ballista/Ballista.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Ballista : EnvironmentHazard
+{
+    [SerializeField]
+    private GameObject projectile;
+    [SerializeField]
+    private Transform projectileStartingPosition;
+
+    protected override void UseEnvironmentHazard() => Instantiate(projectile, projectileStartingPosition.position, Quaternion.identity);
+}
