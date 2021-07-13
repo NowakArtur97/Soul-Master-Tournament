091e7c00017a5d1e89ca348ac1f2d03fcd59ca50
NowakArtur97 <nowart97@gmail.com>
2021-07-13 10:18:34 +0200

    Set max volume on game start

--- Assets/Main Menu/Scripts/VolumeSlider.cs
+++ Assets/Main Menu/Scripts/VolumeSlider.cs
@@ -1,10 +1,22 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class VolumeSlider : MonoBehaviour
 {
     [SerializeField]
+    private Slider _volumeSlider;
+
+    [SerializeField]
     private AudioMixer _audioMixer;
+
+    private void Start() => SetMaxVolume();
+
+    private void SetMaxVolume()
+    {
+        _volumeSlider.value = _volumeSlider.maxValue;
+        SetVolume(_volumeSlider.value);
+    }
 
     public void SetVolume(float volume) => _audioMixer.SetFloat("volume", volume);
 }
