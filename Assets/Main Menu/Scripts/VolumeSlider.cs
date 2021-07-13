d013ca2491b9256d81b981b08134cc44a5abee94
NowakArtur97 <nowart97@gmail.com>
2021-07-13 09:31:48 +0200

    Change audio volume using Slider

---
+++ Assets/Main Menu/Scripts/VolumeSlider.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeSlider : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer _audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        _audioMixer.SetFloat("volume", volume);
+    }
+}
