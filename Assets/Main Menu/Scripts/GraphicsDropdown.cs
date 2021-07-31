8028febf9836b23402cd14039aba535b4907de8b
NowakArtur97 <nowart97@gmail.com>
2021-07-31 08:59:49 +0200

    Set default quality options when starting game

--- Assets/Main Menu/Scripts/GraphicsDropdown.cs
+++ Assets/Main Menu/Scripts/GraphicsDropdown.cs
@@ -1,6 +1,22 @@
+using TMPro;
 using UnityEngine;
 
 public class GraphicsDropdown : MonoBehaviour
 {
+    private const int MAXIMUM_QUALITY_LEVEL = 2;
+
+    [SerializeField]
+    private TMP_Dropdown _graphicsDropdown;
+
+    private void Start() => ChoseDefaultOptions();
+
     public void SetQuality(int qualityIndex) => QualitySettings.SetQualityLevel(qualityIndex);
+
+    private void ChoseDefaultOptions()
+    {
+        QualitySettings.SetQualityLevel(MAXIMUM_QUALITY_LEVEL);
+
+        _graphicsDropdown.value = MAXIMUM_QUALITY_LEVEL;
+        _graphicsDropdown.RefreshShownValue();
+    }
 }
