c898a9a43c035a13ba47a3c10da100ff015bb62d
NowakArtur97 <nowart97@gmail.com>
2021-08-13 08:02:56 +0200

    Stop spawning Soul after every time wall is destroyed

--- Assets/Environment Hazards/Destructible Wall/DestructibleWall.cs
+++ Assets/Environment Hazards/Destructible Wall/DestructibleWall.cs
@@ -8,6 +8,8 @@ public class DestructibleWall : MonoBehaviour
     private Sprite[] _sprites;
     [SerializeField]
     private GameObject _soulSpawner;
+    [SerializeField]
+    private float _chancesForSpawningSoulAfterDestroying = 25;
 
     private void Awake() => GetComponent<SpriteRenderer>().sprite = _sprites[Random.Range(0, _sprites.Length - 1)];
 
@@ -15,8 +17,13 @@ public class DestructibleWall : MonoBehaviour
     {
         if (collision.gameObject.CompareTag(ABILITY_TAG))
         {
-            _soulSpawner.GetComponent<SoulSpawner>().SpawnPickUp(new Vector2((int)transform.position.x, (int)transform.position.y));
+            if (ShouldSpawnSoul())
+            {
+                _soulSpawner.GetComponent<SoulSpawner>().SpawnPickUp(new Vector2((int)transform.position.x, (int)transform.position.y));
+            }
             Destroy(gameObject);
         }
     }
+
+    private bool ShouldSpawnSoul() => Random.Range(0, 100) < _chancesForSpawningSoulAfterDestroying;
 }
