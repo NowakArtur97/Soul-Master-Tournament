0629b10a48ca8e3b7876f750ee2689388383b786
NowakArtur97 <nowart97@gmail.com>
2022-12-01 17:08:31 +0100

    Move sprites and animators to child objects, change colliders and movement for Soul Pick Ups

--- Assets/Scripts/UpAndDownMovement.cs
+++ Assets/Scripts/UpAndDownMovement.cs
@@ -2,19 +2,16 @@ using UnityEngine;
 
 public class UpAndDownMovement : MonoBehaviour
 {
-    [SerializeField]
-    private float _speedDown = 0.5f;
-    [SerializeField]
-    private float _speedUp = 0.5f;
-    [SerializeField]
-    private float _units = 0.3f;
+    [SerializeField] private float _speedDown = 0.5f;
+    [SerializeField] private float _speedUp = 0.5f;
+    [SerializeField] private float _units = 0.3f;
 
     private float _speed;
     private Vector3 _direction;
     private float _min;
     private float _max;
 
-    void Start()
+    private void Start()
     {
         _max = transform.position.y;
         _min = transform.position.y - _units;
@@ -22,7 +19,7 @@ public class UpAndDownMovement : MonoBehaviour
         _direction = Vector3.down;
     }
 
-    void Update()
+    private void Update()
     {
         if (_direction == Vector3.down)
         {
