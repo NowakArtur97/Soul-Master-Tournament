4b90d14cd790c70a4597d62d359dd31aa86afa57
NowakArtur97 <nowart97@gmail.com>
2023-01-20 13:22:01 +0100

    Setup Flamethrower with State Machine

---
+++ Assets/Environment Hazards/Flamethrower/FlamethrowerEntity.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class FlamethrowerEntity : EnvironmentHazardEntity
+{
+    [SerializeField] private D_EnvironmentHazardDealDamageOnContactState _dealDamageOnContactStateData;
+    public D_EnvironmentHazardDealDamageOnContactState DealDamageOnContactStateData
+    {
+        get { return _dealDamageOnContactStateData; }
+        private set { _dealDamageOnContactStateData = value; }
+    }
+
+    protected override void Awake()
+    {
+        IdleState = new IdleForTimeState(this, "idle", IdleStateData);
+        ActiveState = new DealDamageOnAnimationTriggerState(this, "active", _dealDamageOnContactStateData);
+
+        base.Awake();
+    }
+}
