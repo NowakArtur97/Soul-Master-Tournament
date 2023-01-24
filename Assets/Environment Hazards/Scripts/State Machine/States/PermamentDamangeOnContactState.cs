a8146e9c3dfc8ebe0a47646447ebcb0ea0c0a82b
NowakArtur97 <nowart97@gmail.com>
2023-01-24 10:49:56 +0100

    Refactor permament damange on contact state

--- Assets/Environment Hazards/Scripts/State Machine/States/PermamentDamangeOnContactState.cs
+++ Assets/Environment Hazards/Scripts/State Machine/States/PermamentDamangeOnContactState.cs
@@ -1,34 +1,15 @@
-using System.Collections.Generic;
-using UnityEngine;
-
 public class PermamentDamangeOnContactState : ActiveState
 {
     private D_EnvironmentHazardDealDamageOnContactState _dealDamageOnContactStateData;
 
-    private List<GameObject> _allInAgro;
-
     public PermamentDamangeOnContactState(EnvironmentHazardEntity environmentHazardEntity, string animationBoolName,
         D_EnvironmentHazardDealDamageOnContactState dealDamageOnContactStateData)
-        : base(environmentHazardEntity, animationBoolName)
-    {
-        _dealDamageOnContactStateData = dealDamageOnContactStateData;
-        _allInAgro = new List<GameObject>();
-    }
+        : base(environmentHazardEntity, animationBoolName) => _dealDamageOnContactStateData = dealDamageOnContactStateData;
 
     public override void LogicUpdate()
     {
         base.LogicUpdate();
 
-        if (_allInAgro.Count > 0)
-        {
-            DamageAll(_allInAgro);
-        }
-    }
-
-    public override void PhysicsUpdate()
-    {
-        base.PhysicsUpdate();
-
-        _allInAgro = GetAllInMinAgro(_dealDamageOnContactStateData.whatIsDamagable);
+        DamageAll(EnvironmentHazardEntity.ToInteract);
     }
 }
